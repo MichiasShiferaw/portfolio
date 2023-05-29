a2b00aa5b58e88d491d9b5652ec9a0d4cfc99da7
MichiasShiferaw <mshif073@uottawa.ca>
2023-05-28 20:22:45 -0400

    Add data templates and theme functions

---
+++ src/components/data/Projects_data.js
@@ -0,0 +1,112 @@
+export const featuredProject = [
+  {
+    id: 1,
+    keyboard: "",
+    title: "Crypto Scam",
+    type: "Featured Project",
+    summary: "",
+    img: "",
+    link: "",
+    github: "",
+  },
+  {
+    id: 2,
+    keyboard: "",
+    title: "",
+    type: "",
+    summary: "",
+    img: "",
+    link: "",
+    github: "",
+  },
+
+  {
+    id: 3,
+    keyboard: "",
+    title: "",
+    type: "",
+    summary: "",
+    img: "",
+    link: "",
+    github: "",
+  },
+
+  {
+    id: 4,
+    keyboard: "",
+    title: "",
+    type: "",
+    summary: "",
+    img: "",
+    link: "",
+    github: "",
+  },
+
+  {
+    id: 5,
+    keyboard: "",
+    title: "",
+    type: "",
+    summary: "",
+    img: "",
+    link: "",
+    github: "",
+  },
+];
+
+
+export const sideProject = [
+  {
+    id: 1,
+    keyboard: "",
+    title: "Crypto Scam",
+    type: "Featured Project",
+    summary: "",
+    img: "",
+    link: "",
+    github: "",
+  },
+  {
+    id: 2,
+    keyboard: "",
+    title: "",
+    type: "",
+    summary: "",
+    img: "",
+    link: "",
+    github: "",
+  },
+
+  {
+    id: 3,
+    keyboard: "",
+    title: "",
+    type: "",
+    summary: "",
+    img: "",
+    link: "",
+    github: "",
+  },
+
+  {
+    id: 4,
+    keyboard: "",
+    title: "",
+    type: "",
+    summary: "",
+    img: "",
+    link: "",
+    github: "",
+  },
+
+  {
+    id: 5,
+    keyboard: "",
+    title: "",
+    type: "",
+    summary: "",
+    img: "",
+    link: "",
+    github: "",
+  },
+];

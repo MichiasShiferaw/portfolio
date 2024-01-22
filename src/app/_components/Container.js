a587e6c2f6a5decedaca6e2ca62ed99ad961ca5d
Michias Shiferaw <mshif073@uottawa.ca>
2024-01-22 11:21:45 -0500

    Pre good

--- src/app/_components/Container.js
+++ src/app/_components/Container.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Container({ children, ...props }) {
   return (
-    <div className="max-w-screen-lg mx-auto m-auto " {...props}>
+    <div className="max-w-screen-xl mx-auto m-auto " {...props}>
       {children}
     </div>
   );
